70bc406ad57af4e70e464f1fc88ef0f4de9f54f6
gati3478 <giopetriashvili@gmail.com>
2014-07-24 23:22:08 +0400

    Database credetentials
    Database name, username and password. შეცვლის მერე assume-unchanged
    მიეცით.

---
+++ src/hwu/db/DBInfo.java
@@ -0,0 +1,7 @@
+package hwu.db;
+
+public class DBInfo {
+	public static final String DB_NAME = "hwupload";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "passw0rd";
+}
