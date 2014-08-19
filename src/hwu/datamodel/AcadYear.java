e1afc057391a73771a376b1904d929b0e574e8cd
gati3478 <giopetriashvili@gmail.com>
2014-08-19 13:38:55 +0400

    Git is weird. Had to commit.

--- src/hwu/datamodel/AcadYear.java
+++ src/hwu/datamodel/AcadYear.java
@@ -7,22 +7,22 @@ import java.util.List;
 public class AcadYear {
 	private int year;
 	private List<Course> courses;
-	
+
 	public AcadYear(int year) {
 		this.year = year;
 		courses = new ArrayList<Course>();
 	}
-	
+
 	public int getYear() {
 		return year;
 	}
-	
+
 	public void addCourse(Course course) {
 		courses.add(course);
 	}
-	
+
 	public Iterator<Course> iterator() {
 		return courses.iterator();
 	}
-	
+
 }
