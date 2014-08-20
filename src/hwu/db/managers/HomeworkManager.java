9fedddcf706ce62580d65da45cf9c3019d5bec07
gati3478 <giopetriashvili@gmail.com>
2014-08-20 14:49:08 +0400

    Stills needs to be finished

--- src/hwu/db/managers/HomeworkManager.java
+++ src/hwu/db/managers/HomeworkManager.java
@@ -1,10 +1,16 @@
 package hwu.db.managers;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 import hwu.datamodel.Course;
 import hwu.datamodel.Homework;
-import hwu.datamodel.Homework.HomeworkForm;
+import hwu.datamodel.HomeworkForm;
 import hwu.datamodel.users.Lecturer;
 import hwu.datamodel.users.Student;
 import hwu.datamodel.users.User;
@@ -28,16 +34,136 @@ public class HomeworkManager extends Manager {
 	 * 
 	 * @param id
 	 * @return
+	 * @throws SQLException
 	 */
-	public Homework getHomework(int id) {
-		return null;
+	public Homework getHomework(int id) throws SQLException {
+		Homework result = null;
+		Connection con = dataSource.getConnection();
+		String query = "SELECT * FROM homework WHERE id = ?";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(1, id);
+		ResultSet rs = st.executeQuery();
+		// parsing to Homework object
+		if (rs.next()) {
+			String name = rs.getString("name");
+			String description = rs.getString("description");
+			int number = rs.getInt("number");
+			Timestamp deadline = rs.getTimestamp("deadline");
+			boolean active = rs.getBoolean("active");
+			boolean latedaysDisabled = rs.getBoolean("forbid_latedays");
+			result = new Homework(id, name, description, number, deadline,
+					active, latedaysDisabled);
+		}
+		con.close();
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param homework
+	 * @param course
+	 */
+	public void addHomework(Homework homework, Course course) {
+		List<String> columnNames = new ArrayList<String>();
+		columnNames.add("name");
+		columnNames.add("description");
+		columnNames.add("number");
+		columnNames.add("course_id");
+		columnNames.add("deadline");
+		columnNames.add("active");
+		columnNames.add("forbid_latedays");
+		List<String> values = new ArrayList<String>();
+		values.add(homework.getName());
+		values.add(homework.getDescription());
+		values.add("" + homework.getNumber());
+		values.add("" + course.getID());
+		values.add(homework.getDeadline().toString());
+		values.add("" + homework.isActive());
+		values.add("" + homework.latedaysDisabled());
+		executeInsert("homework", columnNames, values);
+	}
+
+	/**
+	 * 
+	 * @param homework
+	 * @param newName
+	 */
+	public void changeHomeworkName(Homework homework, String newName) {
+		executeSimpleUpdate("homework", "name", newName, "id",
+				"" + homework.getID());
+	}
+
+	/**
+	 * 
+	 * @param homework
+	 * @param newDescription
+	 */
+	public void changeHomeworkDescription(Homework homework,
+			String newDescription) {
+		executeSimpleUpdate("homework", "description", newDescription, "id", ""
+				+ homework.getID());
+	}
+
+	/**
+	 * 
+	 * @param homework
+	 * @param newNumber
+	 */
+	public void changeHomeworkNumber(Homework homework, String newNumber) {
+		executeSimpleUpdate("homework", "number", newNumber, "id", ""
+				+ homework.getID());
+	}
+
+	/**
+	 * 
+	 * @param homework
+	 * @param newDeadline
+	 */
+	public void changeHomeworkDeadline(Homework homework, Timestamp newDeadline) {
+		executeSimpleUpdate("homework", "deadline", newDeadline.toString(),
+				"id", "" + homework.getID());
 	}
 
 	/**
 	 * 
 	 * @param homework
 	 */
-	public void addHomework(Homework homework) {
+	public void makeHomeworkActive(Homework homework) {
+		changeHomeworkState(homework, true);
+	}
+
+	/**
+	 * 
+	 * @param homework
+	 */
+	public void makeHomeworkDisabled(Homework homework) {
+		changeHomeworkState(homework, false);
+	}
+
+	private void changeHomeworkState(Homework homework, boolean state) {
+		executeSimpleUpdate("homework", "active", "" + state, "id", ""
+				+ homework.getID());
+	}
+
+	/**
+	 * 
+	 * @param homework
+	 */
+	public void disallowLatedays(Homework homework) {
+		changeLatedaysState(homework, true);
+	}
+
+	/**
+	 * 
+	 * @param homework
+	 */
+	public void allowLatedays(Homework homework) {
+		changeLatedaysState(homework, false);
+	}
+
+	private void changeLatedaysState(Homework homework, boolean state) {
+		executeSimpleUpdate("homework", "forbid_latedays", "" + state, "id", ""
+				+ homework.getID());
 	}
 
 	/**
@@ -45,15 +171,34 @@ public class HomeworkManager extends Manager {
 	 * @param homework
 	 */
 	public void removeHomework(Homework homework) {
+		executeSimpleDelete("homework", "id", homework.getID().toString());
 	}
 
 	/**
 	 * 
 	 * @param homework
 	 * @return
+	 * @throws SQLException
 	 */
-	public List<HomeworkForm> getHomeworkForms(Homework homework) {
-		return null;
+	public List<HomeworkForm> getHomeworkForms(Homework homework)
+			throws SQLException {
+		List<HomeworkForm> forms = new ArrayList<HomeworkForm>();
+		Connection con = dataSource.getConnection();
+		String query = "SELECT * FROM homework_forms WHERE hw_id = ?";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(1, homework.getID());
+		ResultSet rs = st.executeQuery();
+		while (rs.next()) {
+			Integer id = rs.getInt("id");
+			String regex = rs.getString("regex");
+			int maxFileSize = rs.getInt("max_filesize");
+			String fileExt = rs.getString("file_extension");
+			HomeworkForm form = new HomeworkForm(id, regex, maxFileSize,
+					fileExt);
+			forms.add(form);
+		}
+		con.close();
+		return forms;
 	}
 
 	/**
@@ -62,14 +207,57 @@ public class HomeworkManager extends Manager {
 	 * @param form
 	 */
 	public void addHomeworkForm(Homework homework, HomeworkForm form) {
+		List<String> columnNames = new ArrayList<String>();
+		columnNames.add("hw_id");
+		columnNames.add("regex");
+		columnNames.add("max_filesize");
+		columnNames.add("file_extension");
+		List<String> values = new ArrayList<String>();
+		values.add("" + homework.getID());
+		values.add(form.getRegex());
+		values.add("" + form.getMaxFileSize());
+		values.add(form.getFileExtension());
+		executeInsert("homework_forms", columnNames, values);
+	}
+
+	/**
+	 * 
+	 * @param form
+	 * @param newRegex
+	 */
+	public void changeFormRegex(HomeworkForm form, String newRegex) {
+		executeSimpleUpdate("homework_forms", "regex", newRegex, "id", ""
+				+ form.getID());
+	}
+
+	/**
+	 * 
+	 * @param form
+	 * @param newMaxFileSize
+	 */
+	public void changeFormMaxFileSize(HomeworkForm form, int newMaxFileSize) {
+		executeSimpleUpdate("homework_forms", "max_filesize", ""
+				+ newMaxFileSize, "id", "" + form.getID());
+	}
+
+	/**
+	 * 
+	 * @param form
+	 * @param newExt
+	 */
+	public void changeFormFileExtension(HomeworkForm form, String newExt) {
+		executeSimpleUpdate("homework_forms", "file_extension", newExt, "id",
+				"" + form.getID());
 	}
 
 	/**
 	 * 
 	 * @param homework
 	 * @param form
+	 * @throws SQLException
 	 */
 	public void removeHomeworkForm(Homework homework, HomeworkForm form) {
+		executeSimpleDelete("homework_forms", "id", "" + form.getID());
 	}
 
 	/**
