787ba6d42660b41cbaaad7cceded6bebfdd0c1cb
gosipov <gosip12@freeuni.edu.ge>
2014-08-20 15:48:13 +0400

    Course creation done(without enrollment part).

---
+++ WebContent/createcourse.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Create Course</title>
+</head>
+<body>
+<form action="CreateCourse" method="post">
+Name: <input type="text" name="name" maxlength="64" required> <br>
+Description: <br> 
+<textarea rows="4" cols="50" name="descr"></textarea> <br>
+<fieldset>
+  <legend>Starting date </legend>
+  Day: <input type="text" maxlength="2" size="2" name="fday"> 
+  Month: <input type="text" maxlength="2" size="2" name="fmonth"> 
+  Year: <input type="text" maxlength="4" size="4" name="fyear">
+</fieldset>
+<fieldset>
+  <legend>Ending date </legend>
+  Day: <input type="text" maxlength="2" size="2" name="eday"> 
+  Month: <input type="text" maxlength="2" size="2" name="emonth"> 
+  Year: <input type="text" maxlength="4" size="4" name="eyear">
+</fieldset>
+<input class="button" type="submit" value="Continue to enrollment">
+</form>
+</body>
+</html>
---
+++ src/hwu/servlet/CreateCourse.java
@@ -0,0 +1,62 @@
+package hwu.servlet;
+
+import hwu.datamodel.Course;
+import hwu.datamodel.users.Lecturer;
+import hwu.datamodel.users.User;
+import hwu.db.managers.CourseManager;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CreateCourse")
+public class CreateCourse extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public CreateCourse() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User user = (User) request.getSession().getAttribute(User.ATTRIBUTE_NAME);
+		CourseManager manager = (CourseManager) request.getServletContext().
+				getAttribute(CourseManager.ATTRIBUTE_NAME);
+		if (user == null || !(user instanceof Lecturer)) {
+			response.sendRedirect("index.jsp");
+			return;
+		}
+		
+		String name = request.getParameter("name");
+		String description = request.getParameter("descr");
+		Date startDate = constructDate(request.getParameter("fday"), request.getParameter("fmonth"), request.getParameter("fyear"));
+		Date endDate = constructDate(request.getParameter("eday"), request.getParameter("emonth"), request.getParameter("eyear"));
+		if(name == null || description == null || startDate == null || endDate == null){
+			response.sendRedirect("createcourse.jsp");
+			return;
+		}
+		
+	// manager.addCourseToDB(new Course(name, description, startDate, endDate));	
+	}
+	
+	private Date constructDate(String day, String month, String year) {
+		// check whether all fields are filled 
+		if(day == null || month == null || year == null)
+			return null;
+
+		int dayInt = Integer.parseInt(day);
+		int monthInt = Integer.parseInt(month);
+		int yearInt = Integer.parseInt(year);
+		
+		// check whether all fields are filled correctly
+		if(dayInt < 1 || dayInt > 31 || monthInt < 1 || monthInt > 12 || yearInt < 1900) 
+			return null;
+		
+		return Date.valueOf(year + "-" + month + "-" + day);
+	}
+
+}
