ba39460410ccf7213d2ea522a737f81ae87066e3
gosipov <gosip12@freeuni.edu.ge>
2014-08-26 00:27:42 +0400

    Naughty ExcelParser code added.

--- src/hwu/util/ExcelParser.java
+++ src/hwu/util/ExcelParser.java
@@ -6,10 +6,30 @@ import hwu.datamodel.users.User;
 import java.io.File;
 import java.util.List;
 
+// importebi aklia
+
 public class ExcelParser {
 		
-	public static void getStudentList(File file, List<Student> students, List<User> tutors){
-		
+	public static void getStudentList(FileInputStream file, List<Student> students, List<User> tutors) {
+		XSSFWorkbook workbook;
+		try {
+			workbook = new XSSFWorkbook(file);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> rows = sheet.iterator();
+			while(rows.hasNext()) {
+				Iterator<Cell> cells = rows.next().cellIterator();
+				// name; not needed
+				cells.next();
+				// adding student (by e-mail)
+				students.add(new Student(cells.next().getStringCellValue()));
+				// adding tutor if any
+				// Student used as Tutor because it has an e-mail only constructor
+				if(cells.hasNext())	tutors.add(new Student(cells.next().getStringCellValue()));
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
